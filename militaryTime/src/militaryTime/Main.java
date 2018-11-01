cf9e62b2d1733ca407aa243eb5a5bd3793c91b06
Tylor <Tylor@192.168.1.27>
2018-11-01 00:56:50 +0000

    added comments to code

--- militaryTime/src/militaryTime/Main.java
+++ militaryTime/src/militaryTime/Main.java
@@ -1,16 +1,20 @@
 package militaryTime;
 
-
+/*
+ * Tylor Graham
+ * 01/11/18
+ * Reinforcing constructors and .this keyword
+ */
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Times timesObject = new Times(18, 5, 6);
+		Times timesObject = new Times(18, 5, 6);	//creates new object
 		
 
-		System.out.println(timesObject.toMilitary());
-		System.out.println(timesObject.toNorm());
+		System.out.println(timesObject.toMilitary());	//calls the given values to be printed as military time
+		System.out.println(timesObject.toNorm());		//calls the given values to be printed as normal time
 	}
 
 }
--- militaryTime/src/militaryTime/Times.java
+++ militaryTime/src/militaryTime/Times.java
@@ -2,13 +2,13 @@ package militaryTime;
 
 public class Times {
 
-	private int hour;
+	private int hour;		//declares required fields
 	private int minute;
 	private int second;
 	
-	public Times(int hour, int minute, int second) {
+	public Times(int hour, int minute, int second) {	//constructor
 		
-		this.hour = ((hour >=0 && hour<24) ? hour : 0);
+		this.hour = ((hour >=0 && hour<24) ? hour : 0);			//validates times and sets values
 		this.minute = ((minute>=0 && minute<60) ? minute : 0);
 		this.second = ((second >=0 && second <60) ? second : 0);
 		
@@ -17,13 +17,13 @@ public class Times {
 	
 	public String toMilitary() {
 		
-		return String.format("%02d:%02d:%02d", hour, minute, second);
+		return String.format("%02d:%02d:%02d", hour, minute, second);	//outputs formatted military time
 		
 	}
 	
 	public String toNorm() {
 		
-		return String.format("%d:%02d:%02d %s", ((hour == 0 || hour == 12) ? 12 : hour%12), minute, second, ((hour >12) ? "PM" : "AM" ));
+		return String.format("%d:%02d:%02d %s", ((hour == 0 || hour == 12) ? 12 : hour%12), minute, second, ((hour >12) ? "PM" : "AM" ));	//outputs formatted normal time
 		
 	}
 }
