f747134ca5f85a923d7b89992a02805ebb8c01b9
Tylor <Tylor@192.168.1.27>
2018-11-01 14:16:18 +0000

    Testing using mulitle constructors with different amount of parameters

---
+++ militaryTime/src/usingMultiConstructors/Main.java
@@ -0,0 +1,20 @@
+package usingMultiConstructors;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ToMilitary militaryObject = new ToMilitary();
+		ToMilitary militaryObject2 = new ToMilitary(5);
+		ToMilitary militaryObject3 = new ToMilitary(5, 13);
+		ToMilitary militaryObject4 = new ToMilitary(5, 13, 43);
+		
+		
+		System.out.printf("%s\n", militaryObject.toMilitary());
+		System.out.printf("%s\n", militaryObject2.toMilitary());
+		System.out.printf("%s\n", militaryObject3.toMilitary());
+		System.out.printf("%s\n", militaryObject4.toMilitary());
+	}
+}
